90cd064e2e9ee9b644b751cbc8c6b3b6777e6821
hintzetyson <46944160+hintzetyson@users.noreply.github.com>
2020-06-04 15:42:42 -0600

    Added Employee class

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,25 @@
-// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+class Employee {
+    constructor (name, id, email) {
+        this.name = name,
+        this.id = id,
+        this.email = email;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getEmail() {
+        return this.email
+    }
+
+    getPosition() {
+        return 'Employee'
+    }
+}
+
+module.exports = Employee;
