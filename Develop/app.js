82368e4e777073bca03eab52deb1ec76c615c081
hintzetyson <46944160+hintzetyson@users.noreply.github.com>
2020-06-04 17:05:26 -0600

    Added engineer class

--- Develop/app.js
+++ Develop/app.js
@@ -15,7 +15,10 @@ const render = require("./lib/htmlRenderer");
 //Need a team constant to allow for the team members to join
 const team = [];
 
+//First team member will be the manager, who makes the team
 function managerFunction() {
+
+    //Manager questions
     const managerQuestions = [
         {
             type: 'input',
@@ -26,7 +29,7 @@ function managerFunction() {
             type: 'input',
             name: 'managerId',
             message: 'What is your ID number?'
-        }
+        },
         {
             type: 'input',
             name: 'managerEmail',
@@ -39,6 +42,7 @@ function managerFunction() {
         }
     ];
 
+    //Prompt for joining in the team members
     inquirer.prompt(managerQuestions).then(function res() {
         let name = res.managerName;
         let id = res.managerId;
@@ -49,26 +53,42 @@ function managerFunction() {
 
         team.push(manager);
     })
+};
+
+function engineerFunction() {
+    const engineerQuestions = [
+        {
+            type: 'input'
+            name: 'engineerName'
+            message: 'What is the name of the Engineer?'
+        },
+        {
+            type: 'input'
+            name: 'engineerId'
+            message: 'What is the Id number of this Engineer?'
+        },
+        {
+            type: 'input'
+            name: 'engineerEmail'
+            message: 'What is the Engineer\'s email address?'
+        },
+        {
+            type: 'input'
+            name: 'githubUsername'
+            message: 'What is the Engineer\'s Github username?'
+        },
+    ];
+
+    inquirer.prompt(engineerQuestions).then(function res() {
+        let name = res.engineerName;
+        let id = res.engineerId;
+        let email = res.engineerEmail;
+        let github = res.engineerGithub;
+
+        const engineer = new Engineer(name, id, email, github);
+
+        team.push(engineer);
+    })
+
+    
 }
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
